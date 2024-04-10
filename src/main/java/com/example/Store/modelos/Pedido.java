a15f3f213b7ae051213c1684922dfae71bd511ec
camilo castrillon <141888939+camilo150@users.noreply.github.com>
2024-04-10 10:37:16 -0500

    Update Pedido.java

--- src/main/java/com/example/Store/modelos/Pedido.java
+++ src/main/java/com/example/Store/modelos/Pedido.java
@@ -9,15 +9,28 @@ import java.time.LocalDateTime;
 public class Pedido {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-    @Column(name = "FechaYHora",nullable = false,length = 50)
-    private LocalDateTime FechaYHora;//no vacio formato internacional
+    private Integer id_pedido;
+@Column(name ="FechaYHora",nullable = false )
+    private LocalDateTime FechaYHora;//obligatorio y formato internacional
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario",referencedColumnName = "id_usuario")//se respeta el nombre de la tabla con la que me quiero asociar
+    Usuario usuario; // nombre de la tabala con la que me quiero asociarsiempr3e vba a ser primal la relacion muchos y donde esté muchos me paro
 
     public Pedido() {
     }
 
     public Pedido(Integer id, LocalDateTime fechaYHora) {
-        this.id = id;
+        this.id_pedido = id;
+        FechaYHora = fechaYHora;
+    }
+
+
+    public LocalDateTime getFechaYHora() {
+        return FechaYHora;
+    }
+
+    public void setFechaYHora(LocalDateTime fechaYHora) {
         FechaYHora = fechaYHora;
     }
 }
