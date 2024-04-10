9fa6e7b738169e708d7185411fc399c7c21a5c59
camilo castrillon <141888939+camilo150@users.noreply.github.com>
2024-04-10 10:36:13 -0500

    Update TipoPrenda.java

--- src/main/java/com/example/Store/modelos/TipoPrenda.java
+++ src/main/java/com/example/Store/modelos/TipoPrenda.java
@@ -7,14 +7,24 @@ import jakarta.persistence.*;
 public class TipoPrenda {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Integer id_tipoPrenda;
     @Column(name = "nombre",nullable = false,length = 50)
-    private String nombre;//no vacio letras y espacios
+    private String nombre;//no vacido solo letras y espacios long-max 80
+
     public TipoPrenda() {
     }
 
     public TipoPrenda(Integer id, String nombre) {
-        this.id = id;
+        this.id_tipoPrenda = id;
+        this.nombre = nombre;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 }
