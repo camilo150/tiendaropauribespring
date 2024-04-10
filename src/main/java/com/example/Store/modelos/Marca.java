67d32649a2d3256d49e8b1ef7e625df597468a2b
camilo castrillon <141888939+camilo150@users.noreply.github.com>
2024-04-10 10:39:11 -0500

    Update Marca.java

--- src/main/java/com/example/Store/modelos/Marca.java
+++ src/main/java/com/example/Store/modelos/Marca.java
@@ -11,24 +11,57 @@ import java.time.LocalDate;
 public class Marca {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-    @Column(name = "nombreMarca",nullable = false,length = 50)
-    private String nombreMarca;//no vacio y la misma mrd
-    @Column(name = "nit",nullable = false,length = 50)
-    private String nit;//solo numeros y no vacio
-    @Column(name = "anoCreacion",nullable = true,length = 50)
-    private LocalDate anoCreacion;//formato tradicional
-    @Column(name = "sedePrincipal",nullable = true,length = 50)
+    private Integer id_marca;
+    @Column(name = "nombreMarca",nullable = false,length = 25)
+    private String nombreMarca;//obligatorio con espacios long-max 50
+    @Column(name = "nit",nullable = false,length = 25)
+    private String nit;//obligatorio solo numeros long-max 10
+    @Column(name = "anoCreacion",nullable = false)
+    private LocalDate anoCreacion;//obligatorio
+    @Column(name = "sedePrincipal",nullable = false,length = 25)
     private String sedePrincipal;//no se valida
 
     public Marca() {
     }
 
     public Marca(Integer id, String nombreMarca, String nit, LocalDate anoCreacion, String sedePrincipal) {
-        this.id = id;
+        this.id_marca = id;
         this.nombreMarca = nombreMarca;
         this.nit = nit;
         this.anoCreacion = anoCreacion;
+        this.sedePrincipal = sedePrincipal;
+    }
+
+
+    public String getNombreMarca() {
+        return nombreMarca;
+    }
+
+    public void setNombreMarca(String nombreMarca) {
+        this.nombreMarca = nombreMarca;
+    }
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+
+    public LocalDate getAnoCreacion() {
+        return anoCreacion;
+    }
+
+    public void setAnoCreacion(LocalDate anoCreacion) {
+        this.anoCreacion = anoCreacion;
+    }
+
+    public String getSedePrincipal() {
+        return sedePrincipal;
+    }
+
+    public void setSedePrincipal(String sedePrincipal) {
         this.sedePrincipal = sedePrincipal;
     }
 }
