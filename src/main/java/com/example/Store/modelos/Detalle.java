771c49e87e8185f97a571f44964a65149327bff3
camilo castrillon <141888939+camilo150@users.noreply.github.com>
2024-04-10 10:39:33 -0500

    Update Detalle.java

--- src/main/java/com/example/Store/modelos/Detalle.java
+++ src/main/java/com/example/Store/modelos/Detalle.java
@@ -7,18 +7,34 @@ import jakarta.persistence.*;
 public class Detalle {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-    @Column(name = "costoTotal",nullable = false)
-    private Integer costoTotal;//no vacio solo numeros positvips
-    @Column(name = "cantidadProductos",nullable = false)
-    private Integer cantidadProductos;//solonumers positivos
+    private Integer id_detalle;
+    @Column(name = "",nullable = false)
+    private Integer costoTotal;//obligatorio solo numeros pocitivos
+    @Column(name = "",nullable = false)
+    private Integer cantidadProductos;//obligatorio solo numeros pocitivos
 
     public Detalle() {
     }
 
     public Detalle(Integer id, Integer costoTotal, Integer cantidadProductos) {
-        this.id = id;
+        this.id_detalle = id;
         this.costoTotal = costoTotal;
+        this.cantidadProductos = cantidadProductos;
+    }
+
+    public Integer getCostoTotal() {
+        return costoTotal;
+    }
+
+    public void setCostoTotal(Integer costoTotal) {
+        this.costoTotal = costoTotal;
+    }
+
+    public Integer getCantidadProductos() {
+        return cantidadProductos;
+    }
+
+    public void setCantidadProductos(Integer cantidadProductos) {
         this.cantidadProductos = cantidadProductos;
     }
 }
